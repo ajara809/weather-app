99396d9fc7a74caabf7eb9d999d5040a209fb0b0
ajara809 <ajayrahul809@gmail.com>
2025-09-24 22:56:27 +0530

    Updated forecast page with Bootstrap grid and responsive CSS

--- src/Forecast.jsx
+++ src/Forecast.jsx
@@ -1,14 +1,14 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useLocation } from 'react-router-dom';
-
+import "bootstrap/dist/css/bootstrap-grid.min.css";
 import './styles/forecast.css'
 
 function Forecast() {
 
     const [forecast, setForecast] = useState([]);
     const navigate = useNavigate();
-    const [loading,setLoading] = useState(false);
+    const [loading, setLoading] = useState(false);
     const location = useLocation();
     const city = location.state?.city;
 
@@ -23,36 +23,38 @@ function Forecast() {
                 setForecast(fiveDays);
 
             })
-            setTimeout(()=>{
-                setLoading(false);
-            },1000)
-            
+        setTimeout(() => {
+            setLoading(false);
+        }, 1000)
+
     }, [])
 
-    if(loading) return <div id="d_f">Loading Informations....</div>
+    if (loading) return <div id="d_f">Loading Informations....</div>
 
-    const back = () =>{
+    const back = () => {
         navigate('/')
     }
     return (
         <>
-            <div id="body_w2">
+            <div>
                 <h1 id="h_f">5-Days ForeCast</h1>
                 <h1>{city}</h1>
-                <div id="container_f">
-                    {forecast.map((item, index) => (
-                        <div key={index}>
-                            <p id="date">{item.dt_txt}</p>
-                            <div id="cards_f">
-                                <p>{item.main.temp}°C</p>
-                                <p>{item.weather[0].description}</p>
-                                <img
-                                    src={`https://openweathermap.org/img/wn/${item.weather[0].icon}@2x.png`}
-                                    alt={item.weather[0].description}
-                                />
+                <div className="container-fluid">
+                    <div className="row">
+                        {forecast.map((item, index) => (
+                            <div key={index} className="col-lg-4 col-md-6 col-sm-12 mt-3">
+                                <p id="date">{item.dt_txt}</p>
+                                <div id="cards_f">
+                                    <p>{item.main.temp}°C</p>
+                                    <p>{item.weather[0].description}</p>
+                                    <img
+                                        src={`https://openweathermap.org/img/wn/${item.weather[0].icon}@2x.png`}
+                                        alt={item.weather[0].description}
+                                    />
+                                </div>
                             </div>
-                        </div>
-                    ))}
+                        ))}
+                    </div>
                 </div>
                 <button id="but_f" onClick={back} >Back</button>
             </div>
